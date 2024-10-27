74d3d7a5db98418d99b26a4932aa7c48c425e7f8
Sf-05 <maksimkabaev090@gmail.com>
2024-10-27 14:17:03 +0300

    Update test.py
    Написал тесты к isRussian и get_info_from_file

--- test.py
+++ test.py
@@ -20,22 +20,20 @@ class TestFingerCalc(unittest.TestCase):
     '''
     @brief Класс для тестирования корректности функций рассчета кол-ва нажатий.
     '''
-
+    def test_isRussian(self):
+        '''
+        @brief Проверка символа на принадлежность к русскому алфавиту.
+        '''
+        self.assertTrue(fingercalc.isRussian(' '))
+        pass
     def test_get_info_from_file(self):
         '''
         @brief Передать в соответсвующую функцю имя файла (файл небольшой для
         теста) после сравнить правильность полученных результатов с уже
         известными.
         '''
+        self.assertEqual(fingercalc.get_info_from_file(statistic.update),statistic)
         pass
-
-    def test_count_line(self):
-        '''
-        @brief Несколько раз передать в соответсвующую функцию разные строчки.
-        Сравнить полученные результаты с уже известными.
-        '''
-        pass
-
-
-def test_app() -> None:
-    unittest.main()
+        
+    def test_app() -> None:
+        unittest.main()
