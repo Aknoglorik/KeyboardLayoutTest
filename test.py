6cd39f78e7d0049a59dd01ba7abb1d2e8c25ebaa
Sf-05 <maksimkabaev090@gmail.com>
2024-11-06 16:23:07 +0300

    Написан тест к count_to_score

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-
+import config
 import fingercalc
 
 
@@ -39,13 +39,60 @@ class TestFingerCalc(unittest.TestCase):
         '''
         my_stat = fingercalc.get_info_from_file('test1.txt')
         correct_stat = {
-            'ф': 1,
-            'я': 2,
-            'ч': 3,
-            'ц': 4,
+            'а': 1,
+            'б': 2,
+            'в': 3,
+            'г': 4,
+            'д': 5,
+            'е': 6,
+            'ё': 7,
+            'ж': 8,
+            'з': 9,
+            'и': 10,
+            'й': 11,
+            'к': 12,
+            'л': 13,
+            'м': 14,
+            'н': 15,
+            'о': 16,
+            'п': 17,
+            'р': 18,
+            'с': 19,
+            'т': 20,
+            'у': 21,
+            'ф': 22,
+            'х': 23,
+            'ц': 24,
+            'ч': 25,
+            'ш': 26,
+            'щ': 27,
+            'ъ': 28,
+            'ы': 29,
+            'ь': 30,
+            'э': 31,
+            'ю': 32,
+            'я': 33,
         }
         self.assertCountEqual(my_stat, correct_stat)
-
-
+    def test_count_to_score(self):
+        '''
+        @brief Принятие "Раскладки" (load_layout) и статистики нажатия на клавиши
+        (get_info_from_file) и подсчитывание кол-ва нажатий пальцем.
+        '''
+        QWERTY_LAYOUT = config.load_layout(r'testlayouts/qwerty.toml')
+        text = fingercalc.get_info_from_file('testfiles/test.txt')
+        my_date = fingercalc.count_to_score(text, QWERTY_LAYOUT)
+        date = {
+          'lpinky': 0,
+          'lring': 0,
+          'lmiddle': 0,
+          'lindex': 5,
+          'lthumb': 1,
+          'rpinky': 2,
+          'rring': 0,
+          'rmiddle': 0,
+          'rindex': 0,
+        }
+        self.assertEqual(date, my_date)   
 if __name__ == "__main__":
     unittest.main()
